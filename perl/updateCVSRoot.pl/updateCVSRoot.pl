a0e6310f83702a9f83716d68b0db7ce5269273a2
Tim Troxler <troxology@users.noreply.github.com>
2014-04-11 22:02:02 -0400

    Fix updateCVSRoot quotation marks
    Fix errors in single and double quotation marks (smart quotes) pulled in
    from google docs version of the script

--- perl/updateCVSRoot.pl/updateCVSRoot.pl
+++ perl/updateCVSRoot.pl/updateCVSRoot.pl
@@ -1,16 +1,16 @@
 #!/usr/bin/perl -w
 # to run the script type in the following two commands as you see here:
 #cd your/module
-#find . -name ‘Root’ -exec perl ~/updateCVSRoot.pl {} \;
+#find . -name 'Root' -exec perl ~/updateCVSRoot.pl {} \;
 
 my $file = shift;
-my $content = “”;
-open (FILE, $file) || die “Can’t open $file: $!\n”;
+my $content = "";
+open (FILE, $file) || die "Can't open $file: $!\n";
 while (<FILE>)
 {
 	s{oldcvsroot}{newcvsroot}g;
 }
 close(FILE);
-open(FILE,”>$file”) || die “Can’t open $file: $!\n”;
+open(FILE,">$file") || die "Can't open $file: $!\n";
 print File $content;
 close(FILE);
