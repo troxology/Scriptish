dafabfa285040fa40d6fa312f4ec26f3c93fe939
Tim <troxology@users.noreply.github.com>
2021-12-19 20:09:34 -0500

    Add network idle state. Test emulating screen mode as opposed to print css

--- node/pdf.js
+++ node/pdf.js
@@ -11,9 +11,10 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage();
   const address = process.argv[2]
   console.log('Opening page ' + address);
-  await page.goto(address);
+  await page.goto(address, {"waitUntil" : "networkidle0"});
   const title = await page.title();
   console.log('Saving \'' + title + '\' to pdf.');
+  //await page.emulateMediaType('screen');
   await page.pdf({ path: title + '.pdf', format: 'a4' });
   console.log('Shutting down puppeteer browser.');
   await browser.close();
